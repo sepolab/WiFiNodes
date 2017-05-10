8d0faafcae7b5159db9693f7884560145e9e3348
Sebastian Podiago <tringuyen1506@gmail.com>
2017-05-10 14:07:45 +0700

    Update TMAIOT_RelayNodes_1.0.ino

--- TMAIOT_RelayNodes_1.0.ino
+++ TMAIOT_RelayNodes_1.0.ino
@@ -408,17 +408,17 @@ void setup() {
     Serial.println("DONE Setup mqtt sever");
     WiFi.macAddress(mac);
     Serial.print("MAC: ");
-    Serial.print(mac[5],HEX);
-    Serial.print(":");
-    Serial.print(mac[4],HEX);
-    Serial.print(":");
-    Serial.print(mac[3],HEX);
-    Serial.print(":");
-    Serial.print(mac[2],HEX);
+    Serial.print(mac[0],HEX);
     Serial.print(":");
     Serial.print(mac[1],HEX);
     Serial.print(":");
-    Serial.println(mac[0],HEX);
+    Serial.print(mac[2],HEX);
+    Serial.print(":");
+    Serial.print(mac[3],HEX);
+    Serial.print(":");
+    Serial.print(mac[4],HEX);
+    Serial.print(":");
+    Serial.println(mac[5],HEX);
   }
   ledState = HIGH;
 //  digitalWrite(ledPin, ledState);
