1ae92fc3a4e9bd6a9ff013a95004c1e1f8b7c943
Sebastian Podiago <tringuyen1506@gmail.com>
2017-06-07 18:58:12 +0700

    Update TMAiOT_PowerMeasureNode1.0.ino

--- TMAiOT_PowerMeasureNode1.0.ino
+++ TMAiOT_PowerMeasureNode1.0.ino
@@ -1,3 +1,18 @@
+/**
+  TMAiOT SMART OFFICE PROJECT
+  Name: TMAiOT_PowerMeasureNode
+  Purpose: Tracking AC Current and Energy and send to MQTT Broker
+  Status of LED:
+  - Color RED: WiFi Node is starting up, please wait in 10 seconds.
+  - Color BLUE: WiFi Node is working as expectation without error.
+  - Color GREEN: WiFi Node is in Access Point Mode/Direct-WiFi for Configuration
+  - Color PINK: WiFi is in Client Mode, but cannot connect to MQTT Broker Server due to Broker issue.
+  - Color YELLOW: WiFi is in Client Mode, but cannot connect to WiFi due to WiFi Access Point issue.
+  
+  @author Tri Nguyen nductri@tma.com.vn
+  @version 1.0 6/15/17
+*/
+
 #include "EmonLib.h" 
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
@@ -7,10 +22,11 @@
 #include <FS.h>
 #include <PubSubClient.h> //April23
 #include "math.h"
+#include <Adafruit_NeoPixel.h>    //https://github.com/adafruit/Adafruit_NeoPixel
 //-------------------------------
 
 //---------SOFT RESET BUTTON PARAMETERS---------------------
-const int SOFT_RST_PIN =  0;      // SOFT RESET button is map to port 0
+const int SOFT_RST_PIN =  D3;      // SOFT RESET button is map to port 0
 int buttonState;             // the current reading from the input pin
 int lastButtonState = LOW;   // the previous reading from the input pin
 // the following variables are long's because the time, measured in miliseconds,
@@ -19,23 +35,12 @@ long lastDebounceTime = 0;  // the last time the output pin was toggled
 long debounceDelay = 1000;    // the debounce time; increase if the output flickers
 
 //--------LED INDICATION PARAMETERS--------------------
-const int ledPin =  5; // the number of the LED pin
-// Variables will change :
-int ledState = HIGH;             // ledState used to set the LED
-int configureState = 1;   //It is flag to recognize when is in configuration
+#define PINLED D2
 
-unsigned long previousMillis = 0;        // will store last time LED was updated
-unsigned long previousMillis1 = 0;       // will store last time LED was updated
-unsigned long previousMillis2 = 0;
-unsigned long previousMillis5 = 0;
-
-// constants won't change :
-const long interval1000 = 1000;           // interval at which to blink (milliseconds)
-const long interval50 = 50;           // interval at which to blink (milliseconds)
-const long interval100 = 100;           // interval at which to blink (milliseconds)
-const long interval10000 = 10000;           // interval at which to blink (milliseconds)
-const long interval300 = 300;           // interval at which to blink (milliseconds)
-const long interval5000 = 5000;           // interval at which to blink (milliseconds)
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, PINLED, NEO_GRB + NEO_KHZ800);
 
 //----------------WIFI CHECK STATE PAMAMETERS----------------
 int check = 0;
@@ -44,16 +49,16 @@ bool wifiReconnecting = false;
 
 //------------WIFI CONFIGURATION PARAMETERS--------------------
 bool factoryReset = false;
-char APssid[20] = "SePoLab"; //AP is ESP will connect
-char APpassword[20] = "tn17691510";
+char APssid[20] = "UC_VP"; //AP is ESP will connect
+char APpassword[20] = "RAPtor1234!";
 char ssid[] = "PowerMeasureNode"; // ESP in AP mode
 char password[] = "password";
 WiFiClient espClient; //April23
 byte mac[6];
-
+char temptMac[20] = "";
 //--------------MQTT client PRAMETERS--------------
 //define mqtt server default values here, if there are different values in config.json, they are overwritten.
-char mqtt_server[40] = "192.168.1.30"; //April24
+char mqtt_server[40] = "192.168.0.130"; //April24
 char mqtt_port[6] = "1883"; //April24
 char blynk_token[34] = "YOUR_BLYNK_TOKEN";
 //flag for saving data
@@ -62,8 +67,8 @@ PubSubClient client(espClient);//April23
 unsigned long previousMillis3 = 0; //set time for interval publish
 unsigned long previousMillis4 = 0; //set time for interval publish 
 //char mqttClientID[] = ssid;
-char pubTopicGen[40] = "Trihome/tri03/state";
-char subTopicName[40] = "Trihome/tri03/set";
+char pubTopicGen[40] = "SO/R21/powerNode1/state";
+char subTopicName[40] = "SO/R21/powerNode1/set";
 int valueOfSensor = 0;// value that will be published
 char pubMsg[150] = "Hello Server,it is client's 1st message!";//payload of publishing message
 char subMsg[150]; //payload of subcribled message
@@ -86,22 +91,35 @@ bool simulatedDropedWifi = false;
 //void API for cloud command
 
 
-//---------------
-//SendConfirm () will send feedback to MQTT to confirm the command 
-//---------------
+/**
+  Send Confirmation to broker as same as payloaf for retained
+
+  @param char[] payload
+  @return nope
+  @version 1.0
+  @author Tri Nguyen
+  @date June12017
+
+*/
 void sendConfirmtoRetained (char inputString[]) {
   firstTime = false;
   Serial.print("Message send: ");
   Serial.println(inputString);
   client.publish(pubTopicGen, inputString, true);
 }
-//-------------------------------------
-//sendPowerMeasureIndex to update node status
-//There is 2 states of interval
+
+/**
+sendPowerMeasureIndex to update node status
+There is 2 states of interval
+  @param nope
+  @return
 //1_Power Measure
 //2_AC Current
 //3_Voltage
-//------------------------------------
+  @version 1.0
+  @author Tri Nguyen
+  @date June12017
+*/
 void sendPowerMeasureIndex (int intervalSample) {
   double Irms = emon1.calcIrms(1480);  // Calculate Irms only
   double Prms = Irms* 0.23;
@@ -165,12 +183,18 @@ void sendPowerMeasureIndex (int intervalSample) {
         client.publish(pubTopicGen, publishMessage1, true);
   }
 }
-//-------------------------------------
-//keep-alive interval to update node status
-//There is 2 states of interval
-//1_active when ping frequently
-//2_reconnecting when re-connect
-//------------------------------------
+/**
+  keep-alive interval to update node status. There is 2 states of interval
+  1_active when ping frequently
+  2_reconnecting when re-connect
+  @param char [] payload
+  @return nope
+
+  @version 1.0
+  @author Tri Nguyen
+  @date June12017
+
+*/
 void keepAlive (char inputString[]) {
   char publishMessage [100] = "";
   snprintf(publishMessage, 100, "{\"NodeMacAddress\":\"%02X:%02X:%02X:%02X:%02X:%02X\",\"State\":\"%s\"}",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5], inputString);
@@ -180,14 +204,22 @@ void keepAlive (char inputString[]) {
   snprintf(keepAliveTopic, 55,"%s/keepAlive",pubTopicGen);
   client.publish(keepAliveTopic, publishMessage, true);
 }
-//-------- END case 1.2 if NORMAL MODE-------------------------
-//-----void callback description----
-//If the client is used to subscribe to topics, a callback function must be provided in the constructor.
-//This function is called when new messages arrive at the client.
-//topic - the topic the message arrived on (const char[])
-//payload - the message payload (byte array)
-//length - the length of the message payload (unsigned int)
-//-----------------------------------
+/**
+  callback use to read payload of subcribe topic
+  If the client is used to subscribe to topics, a callback function must be provided in the constructor.
+  This function is called when new messages arrive at the client.
+
+  @param:
+    topic - the topic the message arrived on (const char[])
+    payload - the message payload (byte array)
+    length - the length of the message payload (unsigned int)
+  @return nope
+
+  @version 1.0
+  @author Tri Nguyen
+  @date June12017
+
+*/
 void callback(char* topic, byte* payload, unsigned int length) {
   for (int i = 0; i < 30; i++) {
     subMsg[i] = '#';
@@ -219,46 +251,79 @@ void callback(char* topic, byte* payload, unsigned int length) {
       snprintf (temptCommand, 200, "[%s]: NO SYNTAX FOUND", breakedValue);
       keepAlive(temptCommand);
   }
-//----------END case 1.2 of NORMAL MODE -----------------------
 }
+/**
+  callback notifying us of the need to save config
+  @param: nope
+  @return nope
 
-//callback notifying us of the need to save config
+  @version 1.0
+  @author Tri Nguyen
+  @date June12017
+
+*/
 void saveConfigCallback () {
   Serial.println("Should save config");
   shouldSaveConfig = true;
 }
+/**
+  setup_wifi to config wifi after user change setup
+  @param: nope
+  @return nope
 
+  @version 1.0
+  @author Tri Nguyen
+  @date June12017
+
+*/
 void setup_wifi() {
   delay(10);
   // We start by connecting to a WiFi network
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0)); //RED : setup
+  pixels.show();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(APssid);
   WiFi.begin(APssid, APpassword);
 }
 
+/**
+  recoonect to check wifi status and mqtt connection to avoid send message when network is down
+  @param: nope
+  @return nope
+
+  @version 1.0
+  @author Tri Nguyen
+  @date June12017
+
+*/
 void reconnect() {
   // Loop until we're reconnected
-  //  while (!client.connected()) {
   if (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect(ssid)) {
+    if (client.connect(temptMac)) {
       Serial.println("connected");
+      pixels.setPixelColor(0, pixels.Color(0, 0, 255)); // BLUE: done setup - GOOD STATE
+      pixels.show();
       // Once connected, publish an announcement...
-      char temptCommand[] = "Reconnecting";
-      keepAlive(temptCommand);
+      snprintf (pubMsg, 70, "ID: %s; state: Reconnecting...", ssid);
+      client.publish(pubTopicGen, pubMsg,true);
       // ... and resubscribe
       client.subscribe(subTopicName);
+      wifiReconnecting = false;
     } else {
+      pixels.setPixelColor(0, pixels.Color(255, 0, 255)); // PINK: error in connecting MQTT server
+      pixels.show();
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 10 seconds");
+      wifiReconnecting = true;
     }
   }
 }
 
-//-------end Apirl 23 2016------------
+
 void setup() {
   // set the digital pin as output:
   //SET GPIO 0 IS SOFT RESET PIN AND IS INPUT
@@ -272,7 +337,7 @@ void setup() {
 //  if R burden = 120 for Irms max = 19.5 A => calibration = 17
 //  if R burden = 470 for Irms max = 4.9 A => calibration = 5
 
-  pinMode (ledPin, OUTPUT);
+  pixels.begin(); // This initializes the NeoPixel library.
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
   //    WiFiManager wifiManager;
@@ -289,7 +354,8 @@ void setup() {
 
   //--------Read Congiguration file-----------
   Serial.println("mounting FS...");
-
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // RED: SETUP
+  pixels.show();
   if (SPIFFS.begin()) {
     Serial.println("mounted file system");
     if (SPIFFS.exists("/config.json")) {
@@ -352,9 +418,8 @@ void setup() {
     Serial.print(mac[4],HEX);
     Serial.print(":");
     Serial.println(mac[5],HEX);
+    snprintf(temptMac, 20, "%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   }
-  ledState = HIGH;
-  digitalWrite(ledPin, ledState);
   reconnect();
 }
 
@@ -387,8 +452,8 @@ void loop() {
     //sets timeout until configuration portal gets turned off
     //useful to make it all retry or go to sleep
     //in seconds
-    ledState = LOW;
-    digitalWrite(ledPin, ledState);
+    pixels.setPixelColor(0, pixels.Color(0, 255, 0)); //GREEN: WIFI CONFIGURE
+    pixels.show();
     // The extra parameters to be configured (can be either global or just in the setup)
     // After connecting, parameter.getValue() will get you the configured value
     // id/name placeholder/prompt default length
@@ -416,8 +481,6 @@ void loop() {
       Serial.println("failed to connect existing SSID ...");
     }
     else {
-      ledState = HIGH;
-      digitalWrite(ledPin, ledState);
       //read updated parameters
       WiFi.status();
       strcpy(mqtt_server, custom_mqtt_server.getValue());
@@ -446,6 +509,8 @@ void loop() {
         configFile.close();
         Serial.println();
         //end save
+        pixels.setPixelColor(0, pixels.Color(0, 255, 0)); //BLUE GOOD
+        pixels.show();
       }
       delay(1000);
       ESP.reset();
@@ -459,8 +524,9 @@ void loop() {
   if (wifiReconnecting == true) {
     //      if (!wifiManager.autoConnect(SSIDNAME)) {
     if ((simulatedDropedWifi) or (WiFi.status() != WL_CONNECTED)) {
-      Serial.println("failed to connect, trying in seconds....");
       checkWifi = false;
+      pixels.setPixelColor(0, pixels.Color(255, 255, 0)); // RED+GREEN = YELLOW = Wifi DISCONNECTING...
+      pixels.show();
     }
     else {
       Serial.print("connected at IP: ");
@@ -502,8 +568,5 @@ void loop() {
       }
     }
     client.loop();
-  }
-  else {
-    setup_wifi();
   }
 }
